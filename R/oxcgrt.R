4f6004a4b26c241fc7ab47098fa9d290c3d338f2
Ernest Guevarra <ernest@guevarra.io>
2020-07-06 09:57:11 +0100

    initial commit

---
+++ R/oxcgrt.R
@@ -0,0 +1,19 @@
+################################################################################
+#
+#'
+#' An R API to the Oxford COVID-19 Government Response Tracker
+#'
+#' The Oxford COVID-19 Government Response Tracker (OxCGRT) tracks and
+#' compares worldwide government responses to the COVID-19 pandemic rigorously
+#' and consistently. OxCGRT makes available systematic information in a
+#' consistent way, aiding those who require information have access to it
+#' efficiently for their purposes. This package facilitates access to the
+#' OxCGRT data via its API for R users.
+#'
+#' @docType package
+#' @keywords internal
+#' @name oxcgrt
+#'
+#
+################################################################################
+"_PACKAGE"
