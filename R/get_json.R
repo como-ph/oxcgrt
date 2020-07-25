55efa8de3b02974663f32b25e53f43abed66333b
Ernest Guevarra <ernest@guevarra.io>
2020-07-25 09:26:44 +0100

    create get_json functions

--- R/get_json.R
+++ R/get_json.R
@@ -1,4 +1,22 @@
-
+################################################################################
+#
+#'
+#' Get JSON for stringency index data over time
+#'
+#' @param from Start date for stringency index data to be collected. This can go
+#'   as far back as 2020-01-02 (Default). Format <YYYY-MM-DD>.
+#' @param to End data for stringency index data to be collected. This defaults
+#'   to current date. Format <YYYY-MM-DD>
+#'
+#' @return A character object for specified JSON endpoint
+#'
+#' @examples
+#' get_json_time()
+#'
+#' @export
+#'
+#
+################################################################################
 
 get_json_time <- function(from = "2020-01-02",
                           to = as.character(Sys.Date())) {
@@ -8,6 +26,25 @@ get_json_time <- function(from = "2020-01-02",
 }
 
 
+################################################################################
+#
+#'
+#' Get JSON for policy actions data for a specific country for a specific date
+#'
+#' @param ccode ISO 3166-1 alpha-3 country code
+#' @param on Date on which policy actions data is required. This defaults
+#'   to current date. Format <YYYY-MM-DD>.
+#'
+#' @return A character object for specified JSON endpoint
+#'
+#' @examples
+#' get_json_actions(ccode = "AFG", on = as.character(Sys.Date() - 7))
+#'
+#' @export
+#'
+#
+################################################################################
+
 get_json_actions <- function(ccode, on = as.character(Sys.Date())) {
   base <- "https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/actions"
   x <- paste(base, ccode, on, sep = "/")
@@ -15,27 +52,4 @@ get_json_actions <- function(ccode, on = as.character(Sys.Date())) {
 }
 
 
-get_data_time <- function(json) {
-  x <- jsonlite::fromJSON(txt = json, flatten = TRUE)
 
-  y <- unlist(x[["data"]], recursive = FALSE)
-
-  z <- lapply(X = y, FUN = unlist)
-
-  df <- dplyr::bind_rows(z)
-
-  return(df)
-}
-
-
-get_data_actions <- function(json) {
-  x <- jsonlite::fromJSON(txt = json, flatten = TRUE)
-
-  y <- unlist(x[["data"]], recursive = FALSE)
-
-  z <- lapply(X = y, FUN = unlist)
-
-  df <- dplyr::bind_rows(z)
-
-  return(df)
-}
