99df8b4e5fb40fba7525c36f64bac3c3a20197dc
Ernest Guevarra <ernest@guevarra.io>
2020-11-03 22:20:45 +0000

    update tests

--- tests/testthat/test-get_data.R
+++ tests/testthat/test-get_data.R
@@ -1,12 +1,24 @@
-library(dplyr)
+library(magrittr)
+json_time <- get_json_time(from = "2020-07-18", to = "2020-07-25")
+json_action <- get_json_actions(ccode = "Philippines",
+                                from = "2020-07-18",
+                                to = "2020-07-25")
 
-x <- get_json_time(from = "2020-07-18", to = "2020-07-25") %>%
-  get_data_time()
+x <- json_time %>% get_data()
 
 test_that("output is a tibble", {
   expect_is(x, "tbl")
 })
 
+x <- json_action %>% get_data()
+
+test_that("warnings are released", {
+  expect_warning(json_action %>% get_data())
+})
+
+test_that("output is a list", {
+  expect_is(x, "list")
+})
 
 x <- get_json_actions(ccode = "AFG", from = NULL, to = "2020-07-16") %>%
   get_data()
